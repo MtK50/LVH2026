15f2ce5575839f726d8f2fd7c86d3aa39bd20709
unknown <kylian.maillard50@gmail.com>
2025-12-10 19:36:07 +0100

    Oui inshallah ca passe
    dqdq

---
+++ Assets/Scripts/PKFxCycleQualitySettings.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PKFxCycleQualitySettings : MonoBehaviour
+{
+	private bool m_UpdateQualitySetting = false;
+
+	private int m_QualityLevel = 0;
+	private int m_QualityLevelCount = 0;
+	private RenderPipelineAsset m_CurrentRenderPipelineAsset;
+
+	private void Start()
+	{
+		m_CurrentRenderPipelineAsset = QualitySettings.renderPipeline;
+    }
+
+	public void ChangeQualitySettings()
+	{
+		m_QualityLevel = QualitySettings.GetQualityLevel();
+		m_QualityLevelCount = QualitySettings.names.Length;
+		m_UpdateQualitySetting = true;
+	}
+
+	void Update()
+	{
+		if (m_UpdateQualitySetting)
+		{
+			QualitySettings.SetQualityLevel((m_QualityLevel + 1) % (m_QualityLevelCount));
+			if(m_CurrentRenderPipelineAsset != null)
+				GraphicsSettings.defaultRenderPipeline = m_CurrentRenderPipelineAsset;
+            PopcornFX.PKFxManager.UpdateQualityLevels();
+			m_UpdateQualitySetting = false;
+        }
+	}
+}
