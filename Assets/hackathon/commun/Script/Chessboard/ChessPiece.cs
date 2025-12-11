62f53d0fdc012d15dd9785e921a9ddc6da2e19f5
unknown <kylian.maillard50@gmail.com>
2025-12-11 19:30:54 +0100

    Debut d'un system automatique

--- Assets/hackathon/commun/Script/Chessboard/ChessPiece.cs
+++ Assets/hackathon/commun/Script/Chessboard/ChessPiece.cs
@@ -1,4 +1,6 @@
+using Array2DEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public enum PieceType
 {
@@ -21,7 +23,15 @@ public enum PieceTeam
 public class ChessPiece : MonoBehaviour
 {
     [Header("Chess Piece Settings")]
+    public bool isSelectedToMove = false;
     public Vector2 boardPosition;
+    public GameObject debugCube;
+    [Space(10)]
+    // Create a 5x5 matrix of bool to represent possible moves
+    [SerializeField] private Array2DBool shape = null;
+
+
+    [Space(10)]
 
 
     [Tooltip("Automaticaly defined")]
@@ -37,12 +47,79 @@ public class ChessPiece : MonoBehaviour
 
     public void Initialize()
     {
+        debugCube.SetActive(false);
+
         pieceName = gameObject.name;
 
         piece4DS = GetComponentInChildren<unity4dv.Plugin4DS>();
         Debug.Log(piece4DS.SequenceName);
     }
 
+    public void PionTurn(bool isTurn)
+    {
+        isSelectedToMove = isTurn;
+        debugCube.SetActive(isTurn);
+
+        GetComponent<Collider>().enabled = isTurn;
+
+        HighlightPossibleMove();
+
+    }
+    private void HighlightPossibleMove()
+    {
+        // Ne surligner que si cette pièce est sélectionnée
+        if (!isSelectedToMove) return;
+
+        GameManager.Instance.miniChessboard.possibleChessTiles.Clear();
+
+        foreach (ChessTile tile in GameManager.Instance.miniChessboard.chessTiles)
+        {
+            // Calculer le déplacement relatif depuis la position de la pièce
+            int deltaX = (int)tile.position.x - (int)boardPosition.x;
+            int deltaY = (int)tile.position.y - (int)boardPosition.y;
+
+            // Convertir en indices de la matrice shape (le centre = position de la pièce)
+            int shapeX = deltaX + shape.GridSize.x / 2;
+            int shapeY = deltaY + shape.GridSize.y / 2;
+
+            // Vérifier si ce déplacement est dans les limites de la matrice shape
+            bool isInShapeBounds = shapeX >= 0 && shapeX < shape.GridSize.x && 
+                                    shapeY >= 0 && shapeY < shape.GridSize.y;
+
+            // Surligner uniquement les mouvements valides
+            if (isInShapeBounds && shape.GetCell(shapeX, shapeY))
+            {
+
+                tile.GetComponent<Renderer>().material.color = Color.yellow;
+                //tile.centerPosition.SetActive(true);
+                tile.Enable();
 
 
+                if (tile.position == boardPosition)
+                {
+                    tile.GetComponent<Renderer>().material.color = Color.green;
+                }
+                foreach(ChessPiece piece in GameManager.Instance.miniChessboard.chessPieces)
+                {
+                    if(piece.boardPosition == tile.position)
+                    {
+                        // RED - Ennemy piece on possible move tile
+                        if (piece.pieceTeam != this.pieceTeam)
+                        {
+                            tile.GetComponent<Renderer>().material.color = Color.red;
+                        }
+
+                        // BaseColor - Friendly piece on possible move tile
+                        if (piece.pieceTeam == this.pieceTeam)
+                        {
+                            tile.GetComponent<Renderer>().material.color = tile.baseColor;
+                            //tile.centerPosition.SetActive(false);
+                            tile.Disable();
+                        }
+                    }
+                }
+                GameManager.Instance.miniChessboard.possibleChessTiles.Add(tile);
+            }
+        }
+    }
 }
--- Assets/hackathon/commun/Script/GameManager.cs
+++ Assets/hackathon/commun/Script/GameManager.cs
@@ -2,13 +2,85 @@ using Meta.Voice.Net.WebSockets;
 using Oculus.VoiceSDK.UX;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
     static public GameManager Instance;
 
-    [SerializeField] private GameObject player;
-    [SerializeField] private ChessboardManager miniChessboard;
+    [SerializeField] public GameObject player;
+    [SerializeField] public ChessboardManager miniChessboard;
+
+
+
+    private ChessPiece lastSelectedPiece;
+    private bool isBlueTurn = true;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void MakePionPlayMove()
+    {
+        ChessPiece selectedPion = miniChessboard.chessPieces[Random.Range(0, miniChessboard.chessPieces.Count)];
+        if(selectedPion == null || selectedPion.pieceName == "null") { return; }
+
+
+        Debug.Log($"lastSelected {lastSelectedPiece}/selected {selectedPion}/ Count: {miniChessboard.chessPieces.Count}");
+        if(lastSelectedPiece == selectedPion && miniChessboard.chessPieces.Count != 1)
+        {
+
+            MakePionPlayMove(); 
+            return;
+        }
+        
+        foreach(ChessTile tile in miniChessboard.chessTiles)
+        {
+            tile.GetComponent<Renderer>().material.color = tile.baseColor;
+            tile.Disable();
+
+        }
+
+        foreach (ChessPiece piece in miniChessboard.chessPieces)
+        {
+            if(piece.pieceName == selectedPion.pieceName)
+            {
+                piece.PionTurn(true);
+                lastSelectedPiece = piece;
+            }
+            else
+            {
+                piece.PionTurn(false);
+            }
+        }
+    }
+
+
+    public IEnumerator WaitAndNextTurn(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        isBlueTurn = !isBlueTurn;
+        MakePionPlayMove();
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            MakePionPlayMove();
+        }   
+
+    }
 
 
 
